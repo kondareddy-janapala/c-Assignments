361e108b276926830b5e1d49562154f196fa5323
kondareddy-janapala <104086649+kondareddy-janapala@users.noreply.github.com>
2022-05-05 09:55:54 +0530

    Add files via upload

---
+++ Codebase test 1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace code_base_1
+{
+    using System;
+
+    public class TicketsException : Exception
+    {
+        public TicketsException(string message) : base(message)
+        {
+        }
+    }
+    public class Passanger
+    {
+        private int age;
+        private string dateOfTravel;
+        private int noOfTickets;
+        private string name;
+        public Passanger(int age, string dateOfTravel, string name)
+        {
+            this.age = age;
+            this.dateOfTravel = dateOfTravel;
+            this.name = name;
+        }
+        public void TicketBooking(int noOfTickets)
+        {
+            if (noOfTickets > 5)
+            {
+                throw (new TicketsException("cannot book more than 5 tickets"));
+            }
+            else
+            {
+                this.noOfTickets = noOfTickets;
+                Console.WriteLine("Ticket Booked Sussfully");
+            }
+        }
+    }
+    class Test
+    {
+
+        public static void Main(string[] args)
+        {
+            Passanger passenger = new Passanger(25, "05-05-2022", "Konda Reddy");
+            passenger.TicketBooking(5);
+        }
+    }
+}
