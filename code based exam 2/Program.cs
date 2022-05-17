df05c5f447b28d360159857762f0ffb41e54f2a3
kondareddy-janapala <104086649+kondareddy-janapala@users.noreply.github.com>
2022-05-17 18:12:39 +0530

    Add files via upload

---
+++ code based exam 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace code_based_exam_2
+{
+    public delegate int Calc(int value1, int value2);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc add = (a, b) => a + b;
+            Console.WriteLine("Additoin: " + add(10, 20));
+            Calc sub = (a, b) => a - b;
+            Console.WriteLine("Subtraction: " + sub(10, 20));
+            Calc mul = (a, b) => a * b;
+            Console.WriteLine("Multiplication: " + mul(10, 20));
+            Console.ReadLine();     
+        }
+
+    }
+}
