b5cdb6cc1af006f6249341f812a985ddbe212899
kondareddy-janapala <104086649+kondareddy-janapala@users.noreply.github.com>
2022-06-06 09:29:25 +0530

    case study4

---
+++ casestudy4/case study4.sql
@@ -0,0 +1,7 @@
+create table student(no int, name varchar(40), dob date);
+select * from student;
+
+
+create table course(id int, name varchar(40), duration date, fees decimal);
+select * from course;
+
